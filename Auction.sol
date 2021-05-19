8910efc616b61c4686c98a8461baeb64efa966d7
Jerry Pan <Jerrypan2718@berkeley.edu>
2021-05-19 18:39:55 +0800

    lottery

---
+++ Auction.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Lottery{
+    address payable public owner;
+    uint public startBlock;
+    uint public endBlock;
+    string public ipfsHash;
+
+    enum State {Started, Running, Ended, Canceled}
+    State public auctionState;
+
+    uint public highestBindingBid;
+    address payable public highestBidder;
+
+    mapping(address => uint) public bids;
+    uint bidIncrement;
+
+    constructor(){
+        owner = payable(msg.sender);
+        auctionState = State.Running;
+        startBlock = block.number;
+        endBlock
+    }
+
+}
